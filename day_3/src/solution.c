abeb50a9562821c03ccd73407402c6936c7d07d9
Roberts, Benjamin <benjamin.roberts@sky.uk>
2019-12-17 11:41:34 +0000

    WIP get concatenation of strings working for traversing over buffer edge

--- day_3/src/solution.c
+++ day_3/src/solution.c
@@ -7,17 +7,32 @@ int main() {
 
     FILE *input_file;
     char buff[255];
-    char *instruction;
+    char *instruction_p;
     const char delim[2] = ",";
+    char instruction[10];
+    int should_append = 0;
 
     input_file = fopen("input.txt", "r");
 
-    // @TODO: figure out how to stitch together stuff that runs over the 100 byte cut-off
+    // @TODO: figure out how to stitch together stuff that runs over the 255 byte cut-off
     while (fgets(buff, 255, input_file)) {
-        instruction = strtok(buff, delim);
-        while (instruction != NULL) {
+        instruction_p = strtok(buff, delim);
+
+        if ((instruction_p == buff) && (instruction_p != "\n")) {
+            should_append = 1;
+            printf("Yes here's a thing\n");
+        }
+
+        while (instruction_p != NULL) {
+            if (should_append) {
+                printf("Should be appending here\n");
+                strcat(instruction, instruction_p);
+                should_append = 0;
+            } else {
+                strcpy(instruction, instruction_p);
+            }
             printf("%s\n", instruction);
-            instruction = strtok(NULL, delim);
+            instruction_p = strtok(NULL, delim);
         }
     }
 
